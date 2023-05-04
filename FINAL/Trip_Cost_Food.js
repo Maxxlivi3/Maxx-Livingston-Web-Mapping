c4dff0c27f81a1e93f5d3c2789cf4ea584e576d4
Maxx Livingston <123512459+Maxxlivi3@users.noreply.github.com>
2023-05-04 13:38:15 -0500

    Add files via upload

---
+++ FINAL/Trip_Cost_Food.js
@@ -0,0 +1,115 @@
+var money_spent_food = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-91.25691148593455,43.81118038085743]
+      },
+      "properties": {
+        "name": "Waterfront Restaurant and Tavern",
+        "Average $ Spent": 90
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-91.25322179027782, 43.81596038674474 ]
+      },
+      "properties": {
+        "name": "The Freighthouse Restaurant",
+        "Average $ Spent": 67
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-91.21256060273882, 43.87132471189932]
+      },
+      "properties": {
+        "name": "SHOGUN Sushi & Hibachi",
+        "Average $ Spent": 42
+      }
+    },
+      {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-91.25330999109765, 43.811678087510074]
+      },
+      "properties": {
+        "name": "Bennett O' Riley's Pub",
+        "Average $ Spent": 15
+      }
+    },
+      {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-91.1926719297272, 43.81199218681791]
+      },
+      "properties": {
+        "name": "Alpine Inn",
+        "Average $ Spent": 20
+      }
+    },
+      {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-91.23999243157657, 43.817252799511344]
+      },
+      "properties": {
+        "name": "Howie's on La Crosse",
+        "Average $ Spent": 25
+      }
+    },
+      {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-91.25197937575503, 43.811764750238225]
+      },
+      "properties": {
+        "name": "Bodega Brew Pub, Inc.",
+        "Average $ Spent": 30
+      }
+    },
+      {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-91.25553476041236, 43.8134901511864]
+      },
+      "properties": {
+        "name": "The Raddsion Hotel",
+        "Average $ Spent": 95
+      }
+    },
+      {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-91.25416806041233, 43.81484649705006]
+      },
+      "properties": {
+        "name": "Charmant Hotel",
+        "Average $ Spent": 171
+      }
+    },  
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-91.20198780273887,43.869469948229515]
+      },
+      "properties": {
+        "name": "Crooked Pint Ale House",
+        "Average $ Spent": 40
+      }
+    }
+  ]
+};
